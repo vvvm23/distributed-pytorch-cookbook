5bc560965cfe301c84ffa70c513814b34abf5cfd
Alex McKinney <alexmc@graphcore.ai>
2023-08-25 15:23:30 +0100

    adds generation loop to training untested

--- main-single.py
+++ main-single.py
@@ -8,32 +8,65 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-from data import get_dataset, get_tokenizer, transform_dataset
+from data import GPT2Tokenizer, get_dataset, get_tokenizer, transform_dataset
 from models import TransformerDecoderLM
 
+
 def prepare_batch(batch, pad_id, device):
-    sequence_length = batch["input_ids"].shape[1]
     input_ids, attention_mask = (
         batch["input_ids"],
         batch["attention_mask"][:, :-1],
     )
+    sequence_length = input_ids.shape[1]
     input_ids, targets = input_ids[:, :-1].clone(), input_ids[:, 1:].clone()
     targets[targets == pad_id] = -100
     position_ids = (
-        torch.arange(sequence_length - 1)
-        .unsqueeze(0)
-        .repeat(input_ids.shape[0], 1)
+        torch.arange(sequence_length - 1).unsqueeze(0).repeat(input_ids.shape[0], 1)
     )
 
     batch = dict(
         input_ids=input_ids.to(device, non_blocking=True),
         position_ids=position_ids.to(device, non_blocking=True),
-        mask=~attention_mask.to(
-            dtype=torch.bool, device=device, non_blocking=True
-        ),
+        mask=~attention_mask.to(dtype=torch.bool, device=device, non_blocking=True),
     )
     targets = targets.to(device, non_blocking=True)
     return batch, targets
+
+
+def generate(
+    model: TransformerDecoderLM,
+    prompt: str,
+    tokenizer: GPT2Tokenizer,
+    max_new_tokens: int = 20,
+):
+    batch = tokenizer([prompt], truncation=True, max_length=512, return_tensors="pt")
+    input_ids = batch["input_ids"]
+    sequence_length = input_ids.shape[1]
+    position_ids = torch.arange(sequence_length).unsqueeze(0)
+
+    for _ in range(max_new_tokens):
+        logits = model(input_ids=input_ids, position_ids=position_ids)
+        new_token = logits[0, -1].argmax(dim=-1)
+
+        if new_token == tokenizer.eos_token_id:
+            break
+
+        input_ids = torch.cat(
+            [input_ids, torch.tensor(new_token, dtype=input_ids.dtype).unsqueeze(0)],
+            dim=1,
+        )
+        position_ids = torch.cat(
+            [
+                position_ids,
+                torch.tensor(sequence_length, dtype=position_ids.dtype).unsqueeze(0),
+            ],
+            dim=1,
+        )
+
+        sequence_length += 1
+
+    return tokenizer.decode(input_ids[0], skip_special_tokens=True)
+
 
 def main(args: SimpleNamespace):
     batch_size = 32
@@ -42,16 +75,19 @@ def main(args: SimpleNamespace):
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     tokenizer = get_tokenizer()
-    tokenizer.add_special_tokens({"pad_token": "[PAD]"})
+    tokenizer.pad_token_id = 2
 
     model = TransformerDecoderLM(
         dim=256,
         head_dim=32,
         heads=8,
         num_layers=8,
-        vocab_size=tokenizer.vocab_size + 1,
+        vocab_size=tokenizer.vocab_size,
         max_position_embeddings=sequence_length,
     ).to(device)
+
+    if torch.cuda.is_available():
+        model = model.to(torch.bfloat16)
     model.train()
     # model = torch.compile(model)
 
@@ -62,7 +98,9 @@ def main(args: SimpleNamespace):
     train_dataset = transform_dataset(
         train_dataset, tokenizer, max_length=sequence_length
     )
-    validation_dataset = transform_dataset(validation_dataset, tokenizer, max_length=sequence_length)
+    validation_dataset = transform_dataset(
+        validation_dataset, tokenizer, max_length=sequence_length
+    )
 
     train_loader = DataLoader(
         train_dataset,
@@ -71,7 +109,13 @@ def main(args: SimpleNamespace):
         num_workers=4,
         pin_memory=True,
     )
-    validation_loader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=False)
+    validation_loader = DataLoader(
+        validation_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=4,
+        pin_memory=True,
+    )
 
     for ei in range(epochs):
         pb = tqdm(train_loader)
@@ -91,7 +135,9 @@ def main(args: SimpleNamespace):
 
             total_loss += loss.item()
             if i > 0 and not i % 32:
-                pb.set_description(f"[training] Epoch {ei+1}/{epochs} | loss: {total_loss / 32:.3f}")
+                pb.set_description(
+                    f"[training] Epoch {ei+1}/{epochs} | loss: {total_loss / 32:.3f}"
+                )
                 total_loss = 0.0
 
         with torch.no_grad():
@@ -104,17 +150,21 @@ def main(args: SimpleNamespace):
                 logits = model(**batch)
 
                 logits, targets = logits.view(-1, model.vocab_size), targets.view(-1)
-                loss = F.cross_entropy(
-                    logits, targets, ignore_index=-100
-                )
+                loss = F.cross_entropy(logits, targets, ignore_index=-100)
 
-                accuracy = (logits.argmax(dim=-1) == targets).mean() * 100.
+                accuracy = (logits.argmax(dim=-1) == targets).mean() * 100.0
 
                 total_loss += loss.item()
                 accuracy += total_accuracy.item()
 
-                pb.set_description(f"[validation] Epoch {ei+1}/{epochs} | loss: {total_loss / (i+1):.3f}, accuracy: {total_accuracy / (i+1):.2f}")
+                pb.set_description(
+                    f"[validation] Epoch {ei+1}/{epochs} | loss: {total_loss / (i+1):.3f}, accuracy: {total_accuracy / (i+1):.2f}"
+                )
 
+            print("Argmax sampling from model")
+            print(generate(model, "The big brown cat ", tokenizer))
+            print(generate(model, "One day, ", tokenizer))
+            print(generate(model, "She said ", tokenizer))
 
 
 if __name__ == "__main__":
